a71f178484ab9aa9841f972fa0849e5811efb115
bentitataher <bentitataher@gmail.com>
2021-07-02 21:20:37 +0100

    Supprimer entreprise & navigate to edit cmpt ok

--- src/app/admin-entreprise.service.ts
+++ src/app/admin-entreprise.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminEntrepriseService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  // Get All Secteurs
+  getAllService() {
+    return this.http.get("http://localhost:3000/entreprise/admin")
+  }
+
+  // SUpprimer Secteurs
+  deleteService(id) {
+    return this.http.delete("http://localhost:3000/entreprise/admin/" + id)
+  }
+
 }
--- src/app/views/component-admin/entreprise/entreprise-gestion/entreprise-gestion.component.ts
+++ src/app/views/component-admin/entreprise/entreprise-gestion/entreprise-gestion.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AdminEntrepriseService } from '../../../../admin-entreprise.service';
 
 @Component({
   selector: 'app-entreprise-gestion',
@@ -7,11 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntrepriseGestionComponent implements OnInit {
 
+  public entreprise: any;
+
   constructor(
+    private _adminEntrepriseService: AdminEntrepriseService,
+    private router : Router,
   ) { }
 
 
   ngOnInit(): void {
+    this._adminEntrepriseService.getAllService()
+      .subscribe((entrepriseGetted) => {
+        this.entreprise = entrepriseGetted;
+        console.log("Entreprise Getted ==> : ", this.entreprise);
+      })
+  }
+
+  modifierEntreprise(i) {
+    this.router.navigate(['/admin-entreprise-modification', this.entreprise[i]._id])
+  }
+
+  supprimerEntreprise(i) {
+    let id = this.entreprise[i]._id
+    this._adminEntrepriseService.deleteService(id)
+      .subscribe((entrepriseDeleted) => {
+        console.log(entrepriseDeleted);
+        this.ngOnInit();
+      })
   }
 
 
