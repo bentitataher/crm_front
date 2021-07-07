90375599ba5bf085ebd6460ab07ab735c9009ccd
bentitataher <bentitataher@gmail.com>
2021-07-07 15:48:49 +0100

    liste secteurs  ajoutée au cpt admin modif entrse

--- src/app/views/component-admin/entreprise/entreprise-modification/entreprise-modification.component.ts
+++ src/app/views/component-admin/entreprise/entreprise-modification/entreprise-modification.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { AdminEntrepriseService } from '../../../../admin-entreprise.service';
+import { AdminSecteurService } from '../../../../admin-secteur.service';
 
 @Component({
   selector: 'app-entreprise-modification',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntrepriseModificationComponent implements OnInit {
 
-  constructor() { }
+  public secteur: any;
+
+  constructor(
+    private _adminSecteurService: AdminSecteurService,
+    private _adminEntrepriseService : AdminEntrepriseService
+  ) { }
 
   ngOnInit(): void {
+    this._adminSecteurService.getAllService()
+      .subscribe((secteurGetted) => {
+        this.secteur = secteurGetted;
+      })
   }
+
+  adminEditCompanyForm = new FormGroup({
+    entreprise: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    secteur: new FormControl(''),
+    // logo: new FormControl(''),
+    tel: new FormControl(''),
+    fax: new FormControl(''),
+    siegeSocial: new FormControl(''),
+    description: new FormControl('')
+  })
 
 }
